c529b3381840438753cd6c034d0078f1579cee6c
KoeInoue <kjustininoue1996@gmail.com>
2023-02-26 08:06:13 +0900

    #83 improve user fetch

--- src/hooks/useGetAuthUser.tsx
+++ src/hooks/useGetAuthUser.tsx
@@ -1,24 +1,23 @@
 import { useEffect } from 'react'
 
 import { useDispatch, useSelector } from 'react-redux'
-import useSWR from 'swr'
 
 import { userService } from '@/services/userService'
 import { RootState } from '@/store'
 import { userSlice } from '@/store/user'
-import { User } from '@/types/model/user'
 
 export const useGetAuthUser = (userStr: string) => {
   const user = useSelector((state: RootState) => state.user.user)
   const dispatch = useDispatch()
 
-  const { data } = useSWR<{ data: { user: User } }>(userStr, (query) =>
-    userService.getAuthUser(),
-  )
-
   useEffect(() => {
-    dispatch(userSlice.actions.updateUser(data?.data.user))
-  }, [data])
+    if (user) {
+      return
+    }
+    userService.getAuthUser().then(({ data }) => {
+      dispatch(userSlice.actions.updateUser(data.user))
+    })
+  }, [userStr])
 
   return { user }
 }
