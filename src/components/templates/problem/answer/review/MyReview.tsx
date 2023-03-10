bb9bf3ced32c594f5a4cf19fafc18090ece2cdd3
KoeInoue <kjustininoue1996@gmail.com>
2023-03-10 14:37:10 +0900

    #85 edit delete review

--- src/components/templates/problem/answer/review/MyReview.tsx
+++ src/components/templates/problem/answer/review/MyReview.tsx
@@ -1,5 +1,8 @@
-import React, { FC, memo, useEffect, useState } from 'react'
+import React, { FC, memo, useState } from 'react'
 
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever'
+import EditIcon from '@mui/icons-material/Edit'
+import MoreVertIcon from '@mui/icons-material/MoreVert'
 import {
   Box,
   Button,
@@ -7,12 +10,25 @@ import {
   TextField,
   Typography,
   useTheme,
+  IconButton,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Menu,
+  Tooltip,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
 } from '@mui/material'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { ProfileAvatar } from '@/components/parts/common/ProfileAvatar'
 import { reviewService } from '@/services/reviewService'
 import { RootState } from '@/store'
+import { commonSlice } from '@/store/common'
 import { fontSizes } from '@/themes/globalStyles'
 import { Review } from '@/types/model/review'
 
@@ -29,7 +45,15 @@ export const MyReview: FC<Props> = memo(
     const user = useSelector((state: RootState) => state.user.user)
     const theme = useTheme()
     const [myReviewValue, setMyReviewValue] = useState<string>('')
+    const [isAlertOpen, setIsAlertOpen] = React.useState<boolean>(false)
+    const [isEdit, setIsEdit] = useState<boolean>(false)
+    const [anchorElMenu, setAnchorElMenu] = useState<null | HTMLElement>(null)
+    const dispatch = useDispatch()
     const saveOwnReview = () => {
+      if (isEdit) {
+        editOwnReview()
+        return
+      }
       setIsLoading(true)
       reviewService
         .createReview(answerId, myReviewValue)
@@ -43,13 +67,97 @@ export const MyReview: FC<Props> = memo(
         })
     }
 
-    useEffect(() => {
-      // do something
-    }, [])
+    const editOwnReview = () => {
+      setIsLoading(true)
+      reviewService
+        .updateReview(
+          reviews.find((review) => review.userId === user?.id)?.id || '',
+          myReviewValue,
+        )
+        .then(() => {
+          const rs = [...reviews]
+          const r = rs.map((r) => {
+            if (r.userId === user?.id) {
+              r.content = myReviewValue
+            }
+            return r
+          })
+          setReviews(r)
+        })
+        .finally(() => {
+          setIsEdit(false)
+          setIsLoading(false)
+        })
+    }
+
+    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+      setAnchorElMenu(event.currentTarget)
+    }
+    const handleCloseUserMenu = (menu: string) => {
+      switch (menu) {
+        case 'edit':
+          setIsEdit(true)
+          setMyReviewValue(
+            reviews.find((review) => review.userId === user?.id)?.content || '',
+          )
+          break
+        case 'delete':
+          confirmDelete()
+          break
+        default:
+          break
+      }
+
+      setAnchorElMenu(null)
+    }
+
+    const confirmDelete = () => {
+      setIsAlertOpen(true)
+    }
+
+    const handleAlertClose = () => {
+      setIsAlertOpen(false)
+    }
+
+    const deleteReview = () => {
+      handleAlertClose()
+      dispatch(commonSlice.actions.updateIsBackdropShow(true))
+      reviewService
+        .deleteReview(
+          reviews.find((review) => review.userId === user?.id)?.id || '',
+        )
+        .then(() => {
+          const rs = [...reviews]
+          rs.splice(
+            rs.findIndex((review) => review.userId === user?.id),
+            1,
+          )
+          setReviews(rs)
+          dispatch(
+            commonSlice.actions.updateSnackBar({
+              isSnackbarShow: true,
+              snackBarMsg: 'Your review has been deleted',
+              snackBarType: 'success',
+            }),
+          )
+        })
+        .catch(() => {
+          dispatch(
+            commonSlice.actions.updateSnackBar({
+              isSnackbarShow: true,
+              snackBarMsg: 'Failed to delete your review',
+              snackBarType: 'error',
+            }),
+          )
+        })
+        .finally(() =>
+          dispatch(commonSlice.actions.updateIsBackdropShow(false)),
+        )
+    }
 
     return (
       <>
-        {reviews.find((review) => review.userId === user?.id) ? (
+        {reviews.find((review) => review.userId === user?.id) && !isEdit ? (
           <Box sx={{ display: 'flex' }}>
             <ProfileAvatar user={user} />
             <Box
@@ -64,6 +172,75 @@ export const MyReview: FC<Props> = memo(
                 {reviews.find((review) => review.userId === user?.id)?.content}
               </Typography>
             </Box>
+            <Tooltip title="Open menu">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <MoreVertIcon />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElMenu}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElMenu)}
+              onClose={handleCloseUserMenu}
+            >
+              <List
+                sx={{
+                  width: '100%',
+                  maxWidth: 360,
+                  bgcolor: 'background.paper',
+                }}
+                component="nav"
+                aria-labelledby="nested-list-subheader"
+              >
+                <ListItemButton onClick={() => handleCloseUserMenu('edit')}>
+                  <ListItemIcon>
+                    <EditIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Edit" />
+                </ListItemButton>
+                <ListItemButton onClick={() => handleCloseUserMenu('delete')}>
+                  <ListItemIcon>
+                    <DeleteForeverIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Delete" />
+                </ListItemButton>
+              </List>
+            </Menu>
+            <Dialog
+              open={isAlertOpen}
+              onClose={handleAlertClose}
+              aria-labelledby="alert-dialog-title"
+              aria-describedby="alert-dialog-description"
+            >
+              <DialogTitle id="alert-dialog-title">Confirm</DialogTitle>
+              <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                  Are you sure you want to delete this review?
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button color="inherit" onClick={handleAlertClose}>
+                  Cancel
+                </Button>
+                <Button
+                  onClick={() => deleteReview()}
+                  variant="contained"
+                  autoFocus
+                >
+                  Yes, Delete
+                </Button>
+              </DialogActions>
+            </Dialog>
           </Box>
         ) : isLoading ? (
           <CircularProgress />
@@ -86,6 +263,15 @@ export const MyReview: FC<Props> = memo(
             >
               Save
             </Button>
+            {isEdit && (
+              <Button
+                color="inherit"
+                variant="contained"
+                onClick={() => setIsEdit(false)}
+              >
+                Cancel
+              </Button>
+            )}
           </>
         )}
       </>
