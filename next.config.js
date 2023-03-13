cc557c81ec40cb99656c37514e1a0ba00089fc1e
KoeInoue <kjustininoue1996@gmail.com>
2023-03-13 11:54:02 +0900

    #57 closes

--- next.config.js
+++ next.config.js
@@ -7,8 +7,26 @@ const nextConfig = {
     defaultLocale: 'en',
     localeDetection: false,
   },
-  env: {
-    API_URL: process.env.API_URL,
+  asyncheaders() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          { key: 'X-DNS-Prefetch-Control', value: 'on' },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
+          { key: 'X-Frame-Options', value: 'DENY' },
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          {
+            key: 'ContentSecurityPolicy',
+            // eslint-disable-next-line quotes
+            value: "default-src'self';img-src*;",
+          },
+        ],
+      },
+    ]
   },
 }
 
