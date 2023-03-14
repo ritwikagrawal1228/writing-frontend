08b38ad178c1a2926e39941e0f51e179af06e4c9
KoeInoue <kjustininoue1996@gmail.com>
2023-03-14 19:22:31 +0900

    fix backend

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -12,19 +12,6 @@ import awsExports from '@/aws-exports'
 import LpNavBar from '@/components/templates/lp/LpNavBar'
 import { Path } from '@/constants/Path'
 
-const style = {
-  position: 'absolute' as const,
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
-  overflow: 'auto',
-}
-
 Amplify.configure({ ...awsExports, ssr: true })
 
 export default function AuthPage() {
