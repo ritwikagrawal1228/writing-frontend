879dcc6557c9c4039a1906c069ad1b690b34c0ee
KoeInoue <kjustininoue1996@gmail.com>
2023-03-15 11:54:57 +0900

    fix conflict

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,28 +1,19 @@
 export type AmplifyDependentResourcesAttributes = {
-  auth: {
-    ieltswritinghelper755b7dda: {
-      IdentityPoolId: 'string'
-      IdentityPoolName: 'string'
-      UserPoolId: 'string'
-      UserPoolArn: 'string'
-      UserPoolName: 'string'
-      AppClientIDWeb: 'string'
-      AppClientID: 'string'
+    "auth": {
+        "ieltswritinghelper755b7dda": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "iwh": {
+            "BucketName": "string",
+            "Region": "string"
+        }
     }
-  }
-  function: {
-    iwhd1ae75c2CustomMessage: {
-      Name: 'string'
-      Arn: 'string'
-      LambdaExecutionRole: 'string'
-      Region: 'string'
-      LambdaExecutionRoleArn: 'string'
-    }
-  }
-  storage: {
-    iwh: {
-      BucketName: 'string'
-      Region: 'string'
-    }
-  }
 }
