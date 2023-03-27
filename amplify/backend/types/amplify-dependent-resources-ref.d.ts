03cfacdae4352b32e50806d5f9bb9df0a954600c
KoeInoue <kjustininoue1996@gmail.com>
2023-03-27 20:55:16 +0900

    Add google login

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -3,8 +3,10 @@ export type AmplifyDependentResourcesAttributes = {
     "iwh2dcc1fd7": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
